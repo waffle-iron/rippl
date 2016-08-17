fc1f8abeb1485c288a8bd033e8f6325b4894e932
Brian Devins <badevins@gmail.com>
2016-08-17 13:25:36 -0400

    Start an rpc server

--- app/main.go
+++ app/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/ripplxyz/rippl/app/cmd"
+import (
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+
+	"github.com/ripplxyz/rippl/lib/rpc"
+)
 
 func main() {
-	cmd.Execute()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 8080))
+	if err != nil {
+		grpclog.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	srv := server{bus: make(chan rpc.Event)}
+	rpc.RegisterRipplServer(grpcServer, &srv)
+	grpcServer.Serve(lis)
+}
+
+type server struct {
+	bus chan rpc.Event
+}
+
+func (s *server) GetEvents(plugin *rpc.Plugin, stream rpc.Rippl_GetEventsServer) error {
+	fmt.Println(plugin.Name)
+	for i := range s.bus {
+		fmt.Println(i.EventType.String())
+	}
+	return nil
 }
